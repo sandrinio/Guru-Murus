44cc9a3de49d848678476ba373c8d83a74bcfdde
sandrinio <sandro.suladze@gmail.com>
2017-03-24 14:27:05 +0400

    manuals page

--- routes/sm_manuals.js
+++ routes/sm_manuals.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Apps = require('../models/applications')
 
 
 router.get('/manuals/smartphone-manuals', function (req, res) {
-  res.render('client/sm_manuals')
+  Apps.find({}).sort('-date').limit(5).exec(function (err, apps) {
+      if( err ){
+        res.send( err )
+      }else{
+        res.render('client/sm_manuals', { appz: apps })
+      }
+    });
+
 });
 
 
